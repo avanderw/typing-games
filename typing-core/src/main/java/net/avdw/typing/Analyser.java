549126cb525db99308c622247764aa4f64a318e7
Andrew van der Westhuizen <avanderw@gmail.com>
2017-09-19 23:11:45 +0200

    finished basic stats on typing

--- typing-core/src/main/java/net/avdw/typing/Analyser.java
+++ typing-core/src/main/java/net/avdw/typing/Analyser.java
@@ -2,6 +2,7 @@ package net.avdw.typing;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.commons.math3.stat.Frequency;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
 public class Analyser {
@@ -11,39 +12,83 @@ public class Analyser {
 
     final int CHAR_PER_WORD = 5;
     final DescriptiveStatistics avgDelta;
+    final DescriptiveStatistics avgPress;
     final Map<Character, DescriptiveStatistics> avgKeyDelta;
+    final Map<Character, DescriptiveStatistics> avgKeyPress;
+    final Map<Character, Frequency> keyErrorFreq;
+    final Map<Character, Long> pressedKeys;
 
     private long last;
+    private int errorIdx;
 
     Analyser(String presented) {
         this.presented = presented;
-        this.transcribed = "";
+        transcribed = "";
 
         avgDelta = new DescriptiveStatistics();
+        avgPress = new DescriptiveStatistics();
         avgKeyDelta = new HashMap();
+        avgKeyPress = new HashMap();
+        keyErrorFreq = new HashMap();
+        pressedKeys = new HashMap();
+
+        errorIdx = -1;
+
     }
 
     void start() {
         last = System.currentTimeMillis();
     }
 
-    void put(char c) {
+    void keyDown(char c) {
+        if (!pressedKeys.isEmpty()) {
+            keyUp(pressedKeys.keySet().iterator().next());
+        }
+        pressedKeys.put(c, System.currentTimeMillis());
+
         if (transcribed.length() == presented.length()) {
             throw new IllegalStateException("Analyser complete");
         }
 
         long timeToLast = System.currentTimeMillis() - last;
 
-        transcribed += c; //http://www.keybr.com/practice
+        if (presented.charAt(transcribed.length()) == c) {
+            transcribed += c; //http://www.keybr.com/practice
 
-        if (!avgKeyDelta.containsKey(c)) {
-            avgKeyDelta.put(c, new DescriptiveStatistics());
+            if (!avgKeyDelta.containsKey(c)) {
+                avgKeyDelta.put(c, new DescriptiveStatistics());
+            }
+
+            avgDelta.addValue(timeToLast);
+            avgKeyDelta.get(c).addValue(timeToLast);
+            
+            if (transcribed.length() == presented.length()) {
+                pressedKeys.clear();
+            }
+        } else if (errorIdx != transcribed.length()) {
+            if (!keyErrorFreq.containsKey(presented.charAt(transcribed.length()))) {
+                keyErrorFreq.put(presented.charAt(transcribed.length()), new Frequency());
+            }
+
+            keyErrorFreq.get(presented.charAt(transcribed.length())).addValue(c);
+            errorIdx = transcribed.length();
         }
 
-        avgDelta.addValue(timeToLast);
-        avgKeyDelta.get(c).addValue(timeToLast);
-
         last = System.currentTimeMillis();
+    }
+
+    void keyUp(Character c) {
+        if (!pressedKeys.containsKey(c)) {
+            return;
+        }
+        
+        long pressDelta = System.currentTimeMillis() - pressedKeys.remove(c);
+        if (!avgKeyPress.containsKey(c)) {
+            avgKeyPress.put(c, new DescriptiveStatistics());
+        }
+        
+        avgPress.addValue(pressDelta);
+        avgKeyPress.get(c).addValue(pressDelta);
     }
 
     double cps() {
@@ -52,5 +97,10 @@ public class Analyser {
 
     double wpm() {
         return cps() * 60 / CHAR_PER_WORD;
+    }
+
+    double acc() {
+        double totalErr = keyErrorFreq.entrySet().stream().mapToLong(entry -> entry.getValue().getSumFreq()).sum();
+        return 1 - totalErr / transcribed.length();
     }
 }
--- typing-core/src/test/java/net/avdw/typing/AnalyserTest.java
+++ typing-core/src/test/java/net/avdw/typing/AnalyserTest.java
@@ -16,8 +16,8 @@ public class AnalyserTest {
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
-    public void testOneWps() {
-        System.out.println("Testing one word per second");
+    public void testOneWps() throws InterruptedException {
+        System.out.println("testOneWps");
         Analyser analyser = new Analyser("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
         analyser.start();
         ExecutorService executor = Executors.newSingleThreadExecutor();
@@ -25,31 +25,72 @@ public class AnalyserTest {
             try {
                 while (true) {
                     TimeUnit.MILLISECONDS.sleep(1000 / analyser.CHAR_PER_WORD);
-                    analyser.put('a');
+                    analyser.keyDown('a');
                 }
             } catch (InterruptedException ex) {
                 // ignore
             }
         });
 
-        try {
-            int delay = 5;
-            System.out.println(String.format("Simulating typing for %ss", delay));
-            TimeUnit.SECONDS.sleep(delay);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Analyser.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        int delay = 5;
+        System.out.println(String.format("Simulating typing for %ss", delay));
+        TimeUnit.SECONDS.sleep(delay);
         executor.shutdownNow();
 
         assertEquals("Expecting 60 wpm", 60f, Math.round(analyser.wpm()), 1f);
     }
 
-    @Test (expected = IllegalStateException.class)
+    @Test(expected = IllegalStateException.class)
     public void testCompletedAnalyser() {
         System.out.println("testCompletedAnalyser");
         Analyser analyser = new Analyser("a");
         analyser.start();
-        analyser.put('a');
-        analyser.put('a');
+        analyser.keyDown('a');
+        analyser.keyDown('a');
+    }
+
+    @Test
+    public void testErrors() {
+        System.out.println("testErrors");
+        Analyser analyser = new Analyser("The quick brown fox jumped over the brown dog!");
+        analyser.start();
+        String transcribed = "The quick fbrown fox jumped over the jbrown dobdg!";
+        for (int idx = 0; idx < transcribed.length(); idx++) {
+            analyser.keyDown(transcribed.charAt(idx));
+        }
+
+        assertEquals("Should only register error once.", 1, analyser.keyErrorFreq.get('g').getSumFreq());
+        assertTrue(analyser.pressedKeys.isEmpty());
+    }
+
+    @Test
+    public void testAccuracy() {
+        System.out.println("testAccuracy");
+        Analyser analyser = new Analyser("The quick brown fox jumped over the brown dog!");
+        analyser.start();
+        String transcribed = "The quick fbrown fox jumped over the jbrown dobdg!";
+        for (int idx = 0; idx < transcribed.length(); idx++) {
+            analyser.keyDown(transcribed.charAt(idx));
+        }
+
+        assertEquals("Error rate is 93%.", .934, analyser.acc(), 0.001);
+        assertTrue(analyser.pressedKeys.isEmpty());
+    }
+
+    @Test
+    public void testKeyUp() throws InterruptedException {
+        System.out.println("testKeyPress");
+        Analyser analyser = new Analyser("The fiction that the magazine does publish is too often excerpted from novels or imminently forthcoming collections");
+        analyser.start();
+        for (int idx = 0; idx < analyser.presented.length(); idx++) {
+            TimeUnit.MILLISECONDS.sleep(50);
+            analyser.keyDown(analyser.presented.charAt(idx));
+            TimeUnit.MILLISECONDS.sleep(10);
+            analyser.keyUp(analyser.presented.charAt(idx));
+        }
+
+        assertEquals("Average press incorrect.", 10f, analyser.avgPress.getMean(), analyser.avgPress.getStandardDeviation());
+        assertEquals("Average key press incorrect.", 10f, analyser.avgKeyPress.get(' ').getMean(), analyser.avgKeyPress.get(' ').getStandardDeviation());
+        assertTrue(analyser.pressedKeys.isEmpty());
     }
 }
